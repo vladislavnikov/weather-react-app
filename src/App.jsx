75354e7a4baa962cb431f5bc1f5da684592be86e
Vladislav <vladislav.d.nikov04@gmail.com>
2023-12-24 14:43:17 +0200

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import GetCity from './common/city';
-import '../public/styles/App.css';
-import Search from './assets/search.png';
+import React, { useState, useEffect } from "react";
+import GetCity from "./common/city";
+import "../public/styles/App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import clouds from "./assets/cloud.png";
+import sun from "./assets/sun.png";
+import rain from "./assets/rain.png";
+import snowflake from "./assets/snowflake.png";
+import mist from "./assets/mist.png";
+
+const WeatherImages = {
+  Clouds: <img src={clouds} alt="cloud" />,
+  Clear: <img src={sun} alt="sun" />,
+  Rain: <img src={rain} alt="rain" />,
+  Snow: <img src={snowflake} alt="snowflake" />,
+  Mist: <img src={mist} alt="mist" />
+  // Add more weather conditions and images as needed
+};
+
 
 const api = {
-  key: "a829b3fc7620a6e8a58d3691c66e51ef"
+  key: "a829b3fc7620a6e8a58d3691c66e51ef",
 };
 
 const city = await GetCity();
@@ -14,42 +30,47 @@ function App() {
   const [location, setLocation] = useState(city);
 
   useEffect(() => {
-    btnClicked(); 
+    btnClicked();
   }, []);
 
   const btnClicked = () => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api.key}&units=metric`)
-      .then(response => response.json())
-      .then(json => setWeather(json))
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api.key}&units=metric`
+    )
+      .then((response) => response.json())
+      .then((json) => setWeather(json));
 
-      document.getElementById('location').value = '';
-    };
+    document.getElementById("location").value = "";
+  };
 
-      return (
-        <div className="App">
-          <h1>Weather App</h1>
-          <div class="search-wrapper">
-           
-              <input
-              id='location'
-                type="text"
-                placeholder="Enter city/town..."
-                onChange={(e) => setLocation(e.target.value)}
-              />
-              <button className="btn-search" onClick={btnClicked}>S</button>
-           
-          </div>
-          
-          {typeof weather.main !== "undefined" ? (
-            <div className='main-wrapper'>
-              <p>{weather.name}</p>
-              <h2>{Math.floor(weather.main.temp)}°C</h2>
-              <p>{weather.weather[0].main}</p>
-              <p>({weather.weather[0].description})</p>
-            </div>
-          ) : (<span>"No existing town!"</span>)}
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <div className="search-wrapper">
+        <input
+          id="location"
+          type="text"
+          placeholder="Enter city/town..."
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <button className="btn-search" onClick={btnClicked}>
+        <FontAwesomeIcon icon={ faSearch } style={{color: "black"}}/>
+        </button>
       </div>
-      );
+
+      {typeof weather.main !== "undefined" ? (
+          <div className="main-wrapper">
+            <p className="city">{weather.name}</p>
+            {WeatherImages[weather.weather[0].main]}
+            <h2>{Math.floor(weather.main.temp)}°C</h2>
+            <h3>{weather.weather[0].main}</h3>
+            <p className="weather">({weather.weather[0].description})</p>
+          </div>
+      ) : (
+        <span>"No existing town!"</span>
+      )}
+    </div>
+  );
 }
 
 export default App;
